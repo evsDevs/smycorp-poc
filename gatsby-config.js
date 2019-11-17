ec2200fad001b7e6a83c9528c7d302bd5734a6c5
Evelyn Cheung <ev.cheung33@gmail.com>
2019-11-17 18:07:21 -0500

    adding gatsby markdown set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `smycorp-poc/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
